5f355e6447cd20314cdd5ab64f9d6f46b56e69b3
gnoatto <guifgnoatto@hotmail.com>
2024-01-02 02:49:29 -0300

    add negrok

--- src/app/services/axiosClient.ts
+++ src/app/services/axiosClient.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosInstance } from 'axios'
 
 const axiosClient: AxiosInstance = axios.create({
-    baseURL: 'https://localhost:7236/',
+    baseURL: 'https://b115-2804-14d-4c83-8626-1880-ea59-19d0-e07f.ngrok-free.app/',
     timeout: 5000,
+    headers: {
+        "ngrok-skip-browser-warning": "69420"
+    }
 });
 
 export default axiosClient
